f7da9a8d1f74819e58cc5ba133a53c70682b8823
Areef Khan <areef_khan@outlook.com>
2025-09-16 03:23:48 +0100

    page routing and navigation created

--- frontend-project/src/App.jsx
+++ frontend-project/src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
+import Favorites from './pages/Favorites';
 import Home from "./pages/Home"
-//import MovieCard from "./components/MovieCard"
+import {Routes, Route} from "react-router-dom"
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-<>
-  <Home/>
-  </>
-  )
+    <div>
+      <NavBar />
+    <main className="main-content">
+      <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/favorites" element={<Favorites />} />   
+      </Routes>
+  </main>
+  </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend-project/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No favorite movies added yet.</h2>
+        <p>Start adding movies to your favourites</p>
+    </div>
+}
+
+export default Favorites
