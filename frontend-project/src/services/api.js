f05b7214980ecc7d701833eb956e78e8fb275405
Areef Khan <areef_khan@outlook.com>
2025-09-18 00:58:58 +0100

    Search function implemented

--- frontend-project/src/services/api.js
+++ frontend-project/src/services/api.js
@@ -1,15 +1,15 @@
 const API_KEY="0d913126f53506a71e55c07a5f7119de";
-CONST base_url = "https://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async() => {
-    const response = await fetch(`${base_url}/movie/popular?api_key=${API_KEY}`);
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data = await response.json();
     return data.results;
 };
 
 export const searchMovies = async(query) => {
     const response = await fetch(
-        `${base_url}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
             query
         )}`
     );
